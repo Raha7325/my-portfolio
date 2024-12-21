e9522f25867613e9ae3a3791a555dc2d2fcf9f07
Raha7325 <f.keshir25@gmail.com>
2024-12-21 17:18:52 +0330

    create components and use sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Cv from "./pages/Cv"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1 className="bg-red-400 text-3xl text-blue-400">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cv" element={<Cv />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
