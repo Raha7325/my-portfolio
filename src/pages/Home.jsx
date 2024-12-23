1f0fc4ea9a29ed9db8e066f04ff7748133385430
Raha7325 <f.keshir25@gmail.com>
2024-12-23 16:51:14 +0330

    create home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,14 @@
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="flex justify-center items-center h-full">
+            <strong className="text-7xl md:text-9xl linear-color-title">
+                Hi,
+                <br className="block md:hidden" />
+                I'm Raha
+                <br />
+                a FrontEnd Developer
+            </strong>
+        </div>
     )
 }
 
