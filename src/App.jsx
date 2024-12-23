bd80975caad29d596ef4222b498efb13527d5ba3
Raha7325 <f.keshir25@gmail.com>
2024-12-23 17:04:00 +0330

    current path route

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,16 @@ import Contact from "./pages/Contact"
 import Cv from "./pages/Cv"
 function App() {
   const location = useLocation()
-  const backgroundStyle = location.pathname == "/" ? "bg-[url('./assets/images/background-homePage.jpeg')]" : "bg-pink-100"
+  const backgroundStyle = location.pathname == "/my-portfolio" ? "bg-[url('./assets/images/background-homePage.jpeg')]" : "bg-pink-100"
   return (
     <div className={`${backgroundStyle} background flex flex-col min-h-screen overflow-y-scroll`}>
       <Header />
       <main className='flex-grow container mx-auto'>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cv" element={<Cv />} />
+          <Route path="/my-portfolio" element={<Home />} />
+          <Route path="/my-portfolio/about" element={<About />} />
+          <Route path="/my-portfolio/contact" element={<Contact />} />
+          <Route path="/my-portfolio/cv" element={<Cv />} />
         </Routes>
       </main>
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,19 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
     return (
         <nav className="py-3 container mx-auto flex justify-between">
-            <NavLink to="/">
+            <NavLink to="/my-portfolio">
                 <img src={Logo} className="rounded-full object-cover h-[70px]" alt="Faezeh(raha) kashir" width={70} height={70} />
             </NavLink>
             <div className="flex items-center">
                 <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
                     <li>
-                        <NavLink to="/about" className="text-gray-900 font-bold hover:underline">About</NavLink>
+                        <NavLink to="/my-portfolio/about" className="text-gray-900 font-bold hover:underline">About</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/contact" className="text-gray-900 font-bold hover:underline">Contact</NavLink>
+                        <NavLink to="/my-portfolio/contact" className="text-gray-900 font-bold hover:underline">Contact</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/cv" className="text-gray-900 font-bold hover:underline">
+                        <NavLink to="/my-portfolio/cv" className="text-gray-900 font-bold hover:underline">
                             CV</NavLink>
                     </li>
                 </ul>
