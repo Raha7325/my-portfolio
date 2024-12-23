d553eb59e935602850a2a268762a005ba5875415
Raha7325 <f.keshir25@gmail.com>
2024-12-23 16:50:55 +0330

    adding about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,14 @@
 const About = () => {
     return (
-        <div>About</div>
+        <h1 className="mt-auto mx-[35px] text-justify">
+            I'm Raha, a passionate web developer with a love for crafting seamless and engaging user experiences.
+
+            I specialize in designing and building user interfaces (UI) that are not only visually appealing but also highly functional. Currently, I work with React and Vue.js, leveraging these powerful frameworks to bring ideas to life. My journey in web development is fueled by a constant drive to learn, improve, and stay updated with the latest technologies.
+
+            For me, coding is more than just writing lines of code—it's about combining creativity and logic to create something meaningful that can make people's lives easier or more delightful.
+
+            When I'm not coding, you'll probably find me enjoying nature, diving into a good book, or exploring new creative challenges. My ultimate goal is to grow as a developer while contributing to the vibrant and ever-evolving world of the web.
+        </h1>
     )
 }
 
